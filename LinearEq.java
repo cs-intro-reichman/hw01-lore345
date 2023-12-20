37c9ce46d0a3a909a6a3c5e5a8b65be3050a52a5
Loren Bar <lorenbar@Lorens-MacBook-Pro.local>
2023-12-20 16:48:33 +0200

    my hw

--- LinearEq.java
+++ LinearEq.java
@@ -1,7 +1,7 @@
 public class LinearEq {
 public static void main(String[] args) {
 double a = Double.parseDouble(args[0]);
-double b  = Double.parseDouble(args[1]);
+double b = Double.parseDouble(args[1]);
 double c = Double.parseDouble(args[2]);
 double x = (c-b)/a;
 
