04d8c1f15acce83f526fcd0589baf652cb9e5916
Loren Bar <lorenbar@Lorens-MacBook-Pro.local>
2023-12-20 15:46:55 +0200

    my hw

--- Coins.java
+++ Coins.java
@@ -4,7 +4,7 @@ int coins = Integer.parseInt(args[0]);
 int cents = coins % 25;
 int quaters = coins / 25;
 
-System.out.println("Use "+quaters+" quaters and "+cents+ " cents");
+System.out.println("Use "+quarters+" quaters and "+cents+ " cents");
 
 
 }}
